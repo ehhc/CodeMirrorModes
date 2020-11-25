3c15ac67212d3d058c29ecd07317e2a1e34ce4e3
ehhc <ehhc@users.noreply.github.com>
2020-11-25 14:11:08 +0100

    unused console.log removed

--- index.js
+++ index.js
@@ -11,7 +11,6 @@
     CodeMirror.defineMode("codemirrormodes", function (conf) {
         let admonitionMode = CodeMirror.getMode(conf, "admonition");
         let tableMode = CodeMirror.getMode(conf, "table")
-        console.log(tableMode)
         return CodeMirror.overlayMode(admonitionMode, tableMode, true);
     });
 
